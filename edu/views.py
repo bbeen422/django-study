055ef099bdcd282ff1bebd6f06e5924e2c6c2c09
koreanluke <minjae@letscoding.kr>
2023-09-02 14:24:46 +0900

    init

---
+++ edu/views.py
@@ -0,0 +1,8 @@
+from django.views.generic import View
+from django.shortcuts import get_object_or_404, render
+
+class Index(View):
+    template_name = 'index.html'
+
+    def get(self, request):
+        return render(request, self.template_name)
