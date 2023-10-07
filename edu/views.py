4cb05171b7d0eaa6e5b38a1036f37dd1a0e516c4
bbeen422 <hyobeen516@gmail.com>
2023-10-07 14:51:19 +0900

    DB에 저장

--- edu/views.py
+++ edu/views.py
@@ -1,5 +1,6 @@
 from django.views.generic import View
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, render,redirect
+from .models import Feed
 
 class Index(View):
     template_name = 'index.html'
@@ -11,4 +12,22 @@ class TagStudy(View):
     template_name = 'tag_study.html'
 
     def get(self, request):
+        feeds = Feed.objects.all().order_by('id')
+        return render(
+            request, 
+            self.template_name,
+            {'feed_list':feeds}
+            )
+
+class NewContent(View):
+    template_name = 'upload_form.html'
+
+    def get(self, request):
         return render(request, self.template_name)
+
+    def post(self, repuest):
+       param = repuest.POST.get('content', '')
+       print("전달받은 내용:" + param)
+       feed = Feed(content=param)
+       feed.save()
+       return redirect('edu:tag_study')
